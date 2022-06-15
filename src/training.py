7250e49134e9c5a51b20a03c4fa1ebfd7994a9ff
alvaro leandro <leandro0807@live.com>
2022-06-15 17:00:42 -0300

    general good practices and refactorings

--- src/training.py
+++ src/training.py
@@ -1,69 +1,66 @@
+from random import random
+
 import pandas as pd
 import joblib
 import matplotlib.pyplot as plt
+import numpy as np
+
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
-from sklearn.metrics import precision_score, recall_score, f1_score
 
 from sklearn.metrics import auc
 from sklearn.metrics import precision_recall_curve
-from random import random
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-
 
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression, SGDClassifier
-import numpy as np
+from sklearn.linear_model import SGDClassifier
 
 df = pd.read_csv(
-    '/home/alvaro/Downloads/Desafio_Base_Eng_ML/Desafio_Eng_ML/desafiocartola.csv')
+    'data/desafiocartola.csv')
 df.set_index('GLOBO_ID', inplace=True)
 
-lista_columns_drop = ['pro_target']
+list_columns_drop = ['pro_target']
 target = df['pro_target']
 
 long_tail_variables = ['anos_desde_criacao', 'instagram_num', 'facebook_num',
-                       'min_camp', 'interacoes_g1', 'tempo_desperd', 'iteracao_volei', 'iteracao_atletismo']
+                       'min_camp', 'interacoes_g1', 'tempo_desperd',
+                       'iteracao_volei', 'iteracao_atletismo']
 
 
 def get_long_tail(long):
     if long is None or long <= -1:
         return np.nan
-    else:
-        return np.log(1+long)
+
+    return np.log(1+long)
 
 
 for column in long_tail_variables:
     df['log_' + column] = df[column].apply(get_long_tail)
-    lista_columns_drop.append(column)
+    list_columns_drop.append(column)
 
 
 def get_total_pixels(pixels):
-    if (pixels is None) | (type(pixels) != str):
+    if (pixels is None) | (not isinstance(pixels, str)):
         return None
-    elif ('x' not in pixels) | (len(pixels.split('x')) <= 1):
+    if ('x' not in pixels) | (len(pixels.split('x')) <= 1):
         return None
-    return float(pixels.split('x')[0])*float(pixels.split('x')[1])
+    
+    splitted_pixel_values = pixels.split('x')
+    return float(splitted_pixel_values[0])*float(splitted_pixel_values[1])
 
 
 df['total_pixels'] = df['pixels'].apply(get_total_pixels)
-lista_columns_drop.append('pixels')
-
-
-def ceil(x): return 1 if x >= 1 else x
+list_columns_drop.append('pixels')
 
 
 for column in ['avg_3', 'avg_4']:
-    df['ceil_' + column] = df[column].apply(ceil)
-    lista_columns_drop.append(column)
+    df.loc[df[column] >= 1, column] = 1
+    df.rename(columns={column: 'ceil_' + column}, inplace=True)
 
-X = df.drop(lista_columns_drop, axis=1).copy()
+X = df.drop(list_columns_drop, axis=1).copy()
 
 pipe = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='mean')),  # Only numeric columns
@@ -74,18 +71,21 @@ pipe = Pipeline(steps=[
 ]
 )
 
-X_train, X_test, y_train, y_test = train_test_split(X, target, train_size=0.7)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, target, train_size=0.7, random_state=0)
 
 pipe.fit(X_train, y_train)
 
-X_train_ = pipe.named_steps['imputer'].fit_transform(X_train)
-X_train__ = pipe.named_steps['center'].fit_transform(X_train_)
-X_train___ = pipe.named_steps['pca'].fit_transform(X_train__)
+X_train_ = pipe.named_steps['imputer'].transform(X_train)
+X_train__ = pipe.named_steps['center'].transform(X_train_)
+X_train___ = pipe.named_steps['pca'].transform(X_train__)
 y_train_pred_prob = pipe.named_steps['sgd'].predict_proba(X_train___)[:, 1]
 
-X_test_ = pipe.named_steps['imputer'].fit_transform(X_test)
-X_test__ = pipe.named_steps['center'].fit_transform(X_test_)
-X_test___ = pipe.named_steps['pca'].fit_transform(X_test__)
+# avoiding use fit in the test data
+
+X_test_ = pipe.named_steps['imputer'].transform(X_test)  
+X_test__ = pipe.named_steps['center'].transform(X_test_)
+X_test___ = pipe.named_steps['pca'].transform(X_test__)
 y_test_pred_prob = pipe.named_steps['sgd'].predict_proba(X_test___)[:, 1]
 
 
@@ -108,8 +108,8 @@ def roc_pr_curve(y, y_pred_prob, show=True):
     ax.set_title('ROC Curve')
     ax.set_xlabel('False Positive Rate')
     ax.set_ylabel('True Positive Rate')
-    ax.scatter([], [], label='AUC: {:.02f}'.format(
-        roc_auc_score(y, y_pred_prob)), color='#F76800')
+    ax.scatter(
+        [], [], label=f'AUC: {round(roc_auc_score(y, y_pred_prob), 2)}', color='#F76800')
     ax.legend(loc='lower right')
 
     ax = axes[1]
@@ -119,10 +119,10 @@ def roc_pr_curve(y, y_pred_prob, show=True):
     ax.set_title('Precision-Recall Curve')
     ax.set_xlabel('Recall')
     ax.set_ylabel('Precision')
-    ax.scatter([], [], label='Random: {:.02f}'.format(
-        auc(rs_recall, rs_precision)), color='#333333')
-    ax.scatter([], [], label='Model: {:.02f}'.format(
-        auc(ms_recall, ms_precision)), color='#F76800')
+    ax.scatter(
+        [], [], label=f'Random:  {round(auc(rs_recall, rs_precision), 2)}', color='#333333')
+    ax.scatter(
+        [], [], label=f'Model: {round(auc(ms_recall, ms_precision), 2)}', color='#F76800')
     ax.legend(loc='upper right')
 
     if not show:
@@ -132,5 +132,6 @@ def roc_pr_curve(y, y_pred_prob, show=True):
 
 
 fig, ax = roc_pr_curve(y_test, y_test_pred_prob, show=True)
+plt.show()
 
 joblib.dump(pipe, 'propension_model.joblib')
