835cc77718bd521bb811d2622e411e6e6ac0b33c
alvaro leandro <leandro0807@live.com>
2022-06-15 21:47:40 -0300

    refactoring to add transformations inside methods

--- src/training.py
+++ src/training.py
@@ -18,28 +18,22 @@ from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import SGDClassifier
 
-df = pd.read_csv(
-    'data/desafiocartola.csv')
-df.set_index('GLOBO_ID', inplace=True)
 
-list_columns_drop = ['pro_target']
-target = df['pro_target']
-
-long_tail_variables = ['anos_desde_criacao', 'instagram_num', 'facebook_num',
-                       'min_camp', 'interacoes_g1', 'tempo_desperd',
-                       'iteracao_volei', 'iteracao_atletismo']
-
-
-def get_long_tail(long):
-    if long is None or long <= -1:
+def get_log(value):
+    if value is None or value <= -1:
         return np.nan
 
-    return np.log(1+long)
+    return np.log(1+value)
 
 
-for column in long_tail_variables:
-    df['log_' + column] = df[column].apply(get_long_tail)
-    list_columns_drop.append(column)
+def transform_long_tail_variables(df, long_tail_vars, transformation_type='log'):
+    transform_options = {'log': get_log}
+
+    for column in long_tail_vars:
+        df[column] = df[column].apply(transform_options[transformation_type])
+        df.rename(columns={column: 'log_' + column}, inplace=True)
+
+    return df
 
 
 def get_total_pixels(pixels):
@@ -47,46 +41,29 @@ def get_total_pixels(pixels):
         return None
     if ('x' not in pixels) | (len(pixels.split('x')) <= 1):
         return None
-    
+
     splitted_pixel_values = pixels.split('x')
     return float(splitted_pixel_values[0])*float(splitted_pixel_values[1])
 
 
-df['total_pixels'] = df['pixels'].apply(get_total_pixels)
-list_columns_drop.append('pixels')
+def calculate_total_screen_pixels(df):
+    df['pixels'] = df['pixels'].apply(get_total_pixels)
+    df.rename(columns={'pixels': 'total_pixels'}, inplace=True)
+    return df
 
 
-for column in ['avg_3', 'avg_4']:
-    df.loc[df[column] >= 1, column] = 1
-    df.rename(columns={column: 'ceil_' + column}, inplace=True)
+def set_max_value(df, col_list, max_value):
+    df[col_list] = df[col_list].clip(upper=max_value)
+    df.rename(
+        columns={col: 'ceil_'+col for col in col_list}, inplace=True)
+    return df
 
-X = df.drop(list_columns_drop, axis=1).copy()
 
-pipe = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='mean')),  # Only numeric columns
-    ('center', StandardScaler()),
-    ('pca', PCA(n_components=15)),
-    ('sgd', SGDClassifier(loss='log', verbose=5,
-                          early_stopping=True, validation_fraction=0.3))
-]
-)
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, target, train_size=0.7, random_state=0)
-
-pipe.fit(X_train, y_train)
-
-X_train_ = pipe.named_steps['imputer'].transform(X_train)
-X_train__ = pipe.named_steps['center'].transform(X_train_)
-X_train___ = pipe.named_steps['pca'].transform(X_train__)
-y_train_pred_prob = pipe.named_steps['sgd'].predict_proba(X_train___)[:, 1]
-
-# avoiding use fit in the test data
-
-X_test_ = pipe.named_steps['imputer'].transform(X_test)  
-X_test__ = pipe.named_steps['center'].transform(X_test_)
-X_test___ = pipe.named_steps['pca'].transform(X_test__)
-y_test_pred_prob = pipe.named_steps['sgd'].predict_proba(X_test___)[:, 1]
+def get_transformed_data(data, pipe):
+    data_ = pipe.named_steps['imputer'].transform(data)
+    data__ = pipe.named_steps['center'].transform(data_)
+    data___ = pipe.named_steps['pca'].transform(data__)
+    return data___
 
 
 def roc_pr_curve(y, y_pred_prob, show=True):
@@ -130,6 +107,43 @@ def roc_pr_curve(y, y_pred_prob, show=True):
 
     return fig, ax
 
+
+df = pd.read_csv(
+    'data/desafiocartola.csv')
+df.set_index('GLOBO_ID', inplace=True)
+
+target_name = 'pro_target'
+target = df[target_name]
+
+long_tail_variables = ['anos_desde_criacao', 'instagram_num', 'facebook_num',
+                       'min_camp', 'interacoes_g1', 'tempo_desperd',
+                       'iteracao_volei', 'iteracao_atletismo']
+
+df = transform_long_tail_variables(df, long_tail_variables)
+df = calculate_total_screen_pixels(df)
+df = set_max_value(df, ['avg_3', 'avg_4'], 1)
+
+X = df.drop(target_name, axis=1).copy()
+
+pipe = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean')),  # Only numeric columns
+    ('center', StandardScaler()),
+    ('pca', PCA(n_components=15)),
+    ('sgd', SGDClassifier(loss='log', verbose=5,
+                          early_stopping=True, validation_fraction=0.3))
+]
+)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, target, train_size=0.7, random_state=0)
+
+pipe.fit(X_train, y_train)
+
+X_train = get_transformed_data(X_train, pipe)
+y_train_pred_prob = pipe.named_steps['sgd'].predict_proba(X_train)[:, 1]
+
+X_test = get_transformed_data(X_test, pipe)
+y_test_pred_prob = pipe.named_steps['sgd'].predict_proba(X_test)[:, 1]
 
 fig, ax = roc_pr_curve(y_test, y_test_pred_prob, show=True)
 plt.show()
